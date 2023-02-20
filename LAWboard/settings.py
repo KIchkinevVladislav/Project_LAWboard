9b1f4fc025f0a9b1389389de4d1d9b15de169a19
Владислав <kant1755@mail.ru>
2023-02-20 06:13:20 +0300

    requir

--- LAWboard/settings.py
+++ LAWboard/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rw6jogvee!)c7c4v#!d@u8@_trc2ld#y^0%uv0z0fk31ok%=e3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
         "*",
